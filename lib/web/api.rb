d4d94a9e8eda27acf7ce40f190764907a47998a7
Mateu Adsuara <mateuadsuara@gmail.com>
2023-11-08 16:27:55 +0100

    Implement basic API

--- lib/web/api.rb
+++ lib/web/api.rb
@@ -1,8 +1,15 @@
 require 'rack'
+require 'json'
+require 'cgi'
 
 module Web
   class Api
     include Rack
+
+    def initialize(service_class)
+      @service_class = service_class
+      @service = @service_class.new([])
+    end
 
     def call(environment)
       request = Request.new(environment)
@@ -11,11 +18,167 @@ module Web
     end
 
     def handle(request)
-      if request.get? && request.path_info == "/status"
-        return Response.new("ready", 200)
+      [
+        :get_status,
+        :put_cars,
+        :post_journey,
+        :post_dropoff,
+        :post_locate
+      ].each do |endpoint|
+        maybe_response = send(endpoint, request)
+        return maybe_response if maybe_response
       end
 
       Response.new(nil, 404)
+    end
+
+    def get_status(request)
+      return nil unless request.get? && request.path_info == "/status"
+
+      Response.new("ready", 200)
+    end
+
+    def put_cars(request)
+      return nil unless request.put? && request.path_info == "/cars"
+
+      body = request.body.gets
+
+      begin
+        cars = Parser.parse_car_list(body)
+      rescue => e
+        return Response.new(e.message, 400)
+      end
+
+      begin
+        @service = @service_class.new(cars)
+      rescue CarPooling::DuplicateIdError => e
+        return Response.new("duplicate id: #{e.id}", 400)
+      end
+
+      Response.new("", 200)
+    end
+
+    def post_journey(request)
+      return nil unless request.post? && request.path_info == "/journey"
+
+      body = request.body.gets
+
+      begin
+        group = Parser.parse_group(body)
+      rescue => e
+        return Response.new(e.message, 400)
+      end
+
+      begin
+        @service.add_group_journey(group)
+      rescue CarPooling::DuplicateIdError => e
+        return Response.new("duplicate id: #{e.id}", 409)
+      end
+
+      Response.new("", 200)
+    end
+
+    def post_dropoff(request)
+      return nil unless request.post? && request.path_info == "/dropoff"
+
+      body = request.body.gets
+
+      begin
+        id = Parser.parse_id(body)
+      rescue => e
+        return Response.new(e.message, 400)
+      end
+
+      begin
+        @service.dropoff_group_by_id(id)
+      rescue CarPooling::MissingIdError
+        return Response.new("", 404)
+      end
+
+      Response.new("", 200)
+    end
+
+    def post_locate(request)
+      return nil unless request.post? && request.path_info == "/locate"
+
+      body = request.body.gets
+
+      begin
+        id = Parser.parse_id(body)
+      rescue => e
+        return Response.new(e.message, 400)
+      end
+
+      begin
+        car = @service.locate_car_by_group_id(id)
+      rescue CarPooling::MissingIdError
+        return Response.new("", 404)
+      end
+
+      return Response.new("", 204) unless car
+
+      Response.new(car.to_h.to_json, 200)
+    end
+  end
+
+  class Parser
+    def self.parse_json(body)
+      begin
+        return JSON.parse(body)
+      rescue => e
+        raise StandardError.new("invalid json\n#{e.message}")
+      end
+    end
+
+    def self.parse_car_list(body)
+      json = self.parse_json(body)
+
+      raise StandardError.new("expected a list") unless json.kind_of?(Array)
+
+      idx = 0
+      json.map do |c|
+        idx += 1
+        self.parse_car_element(c, idx -1)
+      end
+    end
+
+    def self.parse_car_element(c, idx)
+      raise StandardError.new("expected the element on index #{idx} to be an object") unless c.kind_of?(Hash)
+
+      id, seats = c.values_at("id", "seats")
+      raise StandardError.new("missing id attribute on index #{idx}") unless id
+      raise StandardError.new("missing seats attribute on index #{idx}") unless seats
+
+      CarPooling::Car.new(c)
+    end
+
+    def self.parse_group(body)
+      json = self.parse_json(body)
+      raise StandardError.new("expected an object") unless json.kind_of?(Hash)
+
+      id, people = json.values_at("id", "people")
+      raise StandardError.new("missing id attribute") unless id
+      raise StandardError.new("missing people attribute") unless people
+
+      CarPooling::Group.new(json)
+    end
+
+    def self.parse_id(body)
+      begin
+        params = CGI::parse(body)
+        ids = params["ID"]
+      rescue
+      end
+
+      raise StandardError.new("expected one ID x-www-form-urlencoded parameter") unless ids&.length == 1
+
+      begin
+        id = Integer(ids.first)
+      rescue
+        raise StandardError.new("expected ID to be an integer")
+      end
+
+      id
     end
   end
 end
--- spec/web/api_spec.rb
+++ spec/web/api_spec.rb
@@ -1,14 +1,286 @@
-require 'web/api'
 require 'rack/test'
+require 'json'
+
+require 'web/api'
+require 'car_pooling/service'
 
 RSpec.describe Web::Api do
   include Rack::Test::Methods
 
-  let(:app) { described_class.new }
+  let(:service_class) { spy(CarPooling::Service.class) }
+  let(:service) { spy(CarPooling::Service) }
+  let(:app) { described_class.new(service_class) }
 
-  it 'responds to /status with a 200' do
-    get '/status'
+  before(:each) do
+    allow(service_class).to receive(:new)
+      .and_return(service)
+  end
 
-    expect(last_response.status).to eq(200)
+  describe 'get /status' do
+    it 'responds 200' do
+      get('/status')
+
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+  describe 'put /cars' do
+    def request_load_cars(body)
+      put('/cars', body, "CONTENT_TYPE" => "application/json")
+    end
+
+    it 'responds 200 when registered correctly' do
+      cars = [
+        {
+          id: 1,
+          seats: 4
+        },
+        {
+          id: 2,
+          seats: 6
+        }
+      ]
+      request_load_cars(cars.to_json)
+
+      expect(last_response.status).to eq(200)
+      expect(service_class).to have_received(:new)
+        .with(cars.map{|c| CarPooling::Car.new(c)})
+    end
+
+    it 'responds 400 for invalid json' do
+      request_load_cars('::invalid::')
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("invalid json\nunexpected token at '::invalid::'")
+    end
+
+    it 'responds 400 for other container types' do
+      request_load_cars({}.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("expected a list")
+    end
+
+    it 'responds 400 for other element types' do
+      request_load_cars([true].to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("expected the element on index 0 to be an object")
+    end
+
+    it 'responds 400 for missing id' do
+      cars = [
+        {
+          seats: 4
+        }
+      ]
+      request_load_cars(cars.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("missing id attribute on index 0")
+    end
+
+    it 'responds 400 for missing seats' do
+      cars = [
+        {
+          id: 1
+        }
+      ]
+      request_load_cars(cars.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("missing seats attribute on index 0")
+    end
+
+    it 'responds 400 for duplicate ids' do
+      cars = [
+        {
+          id: 1,
+          seats: 4
+        },
+        {
+          id: 1,
+          seats: 6
+        }
+      ]
+      allow(service_class).to receive(:new) do |cars|
+        raise CarPooling::DuplicateIdError.new(id: 1) if cars != []
+      end
+
+      request_load_cars(cars.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("duplicate id: 1")
+    end
+  end
+
+  describe 'post /journey' do
+    def request_perform_journey(body)
+      post('/journey', body, "CONTENT_TYPE" => "application/json")
+    end
+
+    it 'responds 200 when registered correctly' do
+      group = {
+        id: 1,
+        people: 4
+      }
+      request_perform_journey(group.to_json)
+
+      expect(service).to have_received(:add_group_journey)
+        .with(CarPooling::Group.new(group))
+
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'responds 400 for invalid json' do
+      request_perform_journey('::invalid::')
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("invalid json\nunexpected token at '::invalid::'")
+    end
+
+    it 'responds 400 for other types' do
+      request_perform_journey([].to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("expected an object")
+    end
+
+    it 'responds 400 for missing id' do
+      group = {
+        people: 4
+      }
+      request_perform_journey(group.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("missing id attribute")
+    end
+
+    it 'responds 400 for missing people' do
+      group = {
+        id: 1
+      }
+      request_perform_journey(group.to_json)
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("missing people attribute")
+    end
+
+    it 'responds 409 for duplicate ids' do
+      group = {
+        id: 1,
+        people: 4
+      }
+      allow(service).to receive(:add_group_journey)
+        .and_raise(CarPooling::DuplicateIdError.new(id: 1))
+
+      request_perform_journey(group.to_json)
+
+      expect(last_response.status).to eq(409)
+      expect(last_response.body).to eq("duplicate id: 1")
+    end
+  end
+
+  describe 'post /dropoff' do
+    def request_dropoff(body)
+      post('/dropoff', body, "CONTENT_TYPE" => "application/x-www-form-urlencoded")
+    end
+
+    it 'responds 200 when unregistered correctly' do
+      id = 1
+      request_dropoff("ID=#{id}")
+
+      expect(service).to have_received(:dropoff_group_by_id)
+        .with(id)
+
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'responds 400 when not only one id' do
+      ['', 'ID=1&ID=2'].each do |body|
+        request_dropoff(body)
+
+        expect(last_response.status).to eq(400)
+        expect(last_response.body).to eq("expected one ID x-www-form-urlencoded parameter")
+      end
+    end
+
+    it 'responds 400 when invalid id type' do
+      request_dropoff('ID=a')
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("expected ID to be an integer")
+    end
+
+    it 'responds 404 when not found' do
+      id = 1
+      allow(service).to receive(:dropoff_group_by_id)
+        .with(id)
+        .and_raise(CarPooling::MissingIdError.new(id: id))
+
+      request_dropoff("ID=#{id}")
+
+      expect(last_response.status).to eq(404)
+    end
+  end
+
+  describe 'post /locate' do
+    def request_locate(body)
+      post('/locate', body, "CONTENT_TYPE" => "application/x-www-form-urlencoded")
+    end
+
+    it 'responds 200 with the car' do
+      id = 1
+
+      car = CarPooling::Car.new(id: 2, seats: 4)
+      allow(service).to receive(:locate_car_by_group_id)
+        .with(id)
+        .and_return(car)
+
+      request_locate("ID=#{id}")
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq(car.to_h.to_json)
+    end
+
+    it 'responds 400 when not only one id' do
+      ['', 'ID=1&ID=2'].each do |body|
+        request_locate(body)
+
+        expect(last_response.status).to eq(400)
+        expect(last_response.body).to eq("expected one ID x-www-form-urlencoded parameter")
+      end
+    end
+
+    it 'responds 400 when invalid id type' do
+      request_locate('ID=a')
+
+      expect(last_response.status).to eq(400)
+      expect(last_response.body).to eq("expected ID to be an integer")
+    end
+
+    it 'responds 404 when the group is not found' do
+      id = 1
+
+      allow(service).to receive(:locate_car_by_group_id)
+        .with(id)
+        .and_raise(CarPooling::MissingIdError.new(id: id))
+
+      request_locate("ID=#{id}")
+
+      expect(last_response.status).to eq(404)
+    end
+
+    it 'responds 204 when no assigned car' do
+      id = 1
+
+      allow(service).to receive(:locate_car_by_group_id)
+        .with(id)
+        .and_return(nil)
+
+      request_locate("ID=#{id}")
+
+      expect(last_response.status).to eq(204)
+    end
   end
 end
